9f3c191b786905b5abd9ebb130bd4aef2f49c5fc
Vatsal1805 <vatsalbhavsar2011@gmail.com>
2025-10-28 12:16:59 +0530

    feat: add Vercel analytics and speed insights to the application

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 import './App.css'
 import AppRoutes from './routes/AppRoutes.jsx';
@@ -8,8 +10,9 @@ function App() {
 
   return (
     <>
-    <AppRoutes/>
-      
+      <AppRoutes/>
+      <Analytics />
+      <SpeedInsights />
     </>
   )
 }
